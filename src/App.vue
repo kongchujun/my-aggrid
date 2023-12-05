69bcc47d7d5e8126a68d65ce284984caf6c56ed0
chujun <985145157@qq.com>
2023-12-06 00:13:06 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <ag-grid-vue
+      style="width: 500px; height: 300px;"
+      class="ag-theme-alpine"
+      :gridOptions="gridOptions"
+      :rowData="rowData"
+    ></ag-grid-vue>
+  </div>
+</template>
+
+<script>
+import { AgGridVue } from 'ag-grid-vue';
+
+export default {
+  components: {
+    'ag-grid-vue': AgGridVue,
+  },
+  data() {
+    return {
+      gridOptions: {
+        columnDefs: [
+          { headerName: 'Make', field: 'make' },
+          { headerName: 'Model', field: 'model' },
+          { headerName: 'Price', field: 'price' },
+        ],
+      },
+      rowData: [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxster', price: 72000 },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
